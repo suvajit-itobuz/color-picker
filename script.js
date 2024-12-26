1650de2df6c5041c9ef4f8804df0e495f5ed3a08
Suvajit Saha <suvajit@itobuz.com>
2024-12-26 18:41:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const aquaBtn=document.getElementById("aqua-btn");
+const yellowBtn=document.getElementById("yellow-btn");
+const blueBtn=document.getElementById("blue-btn");
+const greenBtn=document.getElementById("green-btn");
+const aquaclick =()=>{
+document.body.style.backgroundColor="aqua";
+}
+const blueclick =()=>{
+document.body.style.backgroundColor="blue";
+}
+const greenclick =()=>{
+document.body.style.backgroundColor="green";
+}
+const yellowclick =()=>{
+document.body.style.backgroundColor="yellow";
+}
